c835e338d19947ad3f6447917b7bc00434c9b320
Tmoradi <tiam.moradi34@gmail.com>
2022-10-30 17:01:12 -0400

    adding script to download datasets from s3 bucket. added our dataset to dvc including having a dvc remote to the s3 bucket for data versioning.

--- utils/download_data.py
+++ utils/download_data.py
@@ -1,22 +1,27 @@
 # Importing packages
-import boto3 
+import boto3
+import os
+def download_data(Prefix:list[str]) -> None:
+    ''' Here we are downloading our dataset from the given bucket 
 
-
-
-# def download_data(bucket: str,destination:str) -> None:
-#     ''' Here we are downloading our dataset from the given bucket 
-
-#         args: 
-#             bucket(str): location of where our dataset is. 
-#             destination(str): 
-#     '''
-
-if __name__ == "__main__":
-
+        args: 
+            bucket(list[str]): location of where our dataset is. 
+            destination(list[str]):  
+    ''' 
     s3 = boto3.resource('s3')
     bucket = s3.Bucket('mle9-spotify-wakeword-data')
     
-    for obj in bucket.objects.filter(Prefix="train"):
-        print(obj.key)
-    # for bucket in s3.buckets.all():
-    #     print(bucket.name)
+    for prefix in Prefix: 
+        for obj in bucket.objects.filter(Prefix=prefix):
+            # Here we are copying the directory structure from s3 bucket.
+            if not os.path.exists(os.path.join('data',os.path.dirname(obj.key))):
+                os.makedirs(os.path.join('data',os.path.dirname(obj.key)))
+            
+            dest_path = os.path.join('data',obj.key)
+            try:
+                bucket.download_file(obj.key,dest_path)
+            except:
+                print(f"We cannot download{obj.key} from {prefix}!")
+
+if __name__ == "__main__":
+    download_data(Prefix=['train'])
