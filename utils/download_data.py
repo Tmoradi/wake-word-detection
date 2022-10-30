ca7c7f97c67756746ba9a87ff5f5c95c0778a7d4
Tmoradi <tiam.moradi34@gmail.com>
2022-10-30 17:11:58 -0400

    updating docstring and naming of variables in download data function

--- utils/download_data.py
+++ utils/download_data.py
@@ -1,22 +1,23 @@
 # Importing packages
 import boto3
 import os
-def download_data(Prefix:list[str]) -> None:
+
+def download_data(prefix_list:list[str]) -> None:
     ''' Here we are downloading our dataset from the given bucket 
 
         args: 
-            bucket(list[str]): location of where our dataset is. 
-            destination(list[str]):  
+            Prefix(list[str]): subdirectories inside of our main bucket in s3.
+            may want to include functionality for 
     ''' 
     s3 = boto3.resource('s3')
     bucket = s3.Bucket('mle9-spotify-wakeword-data')
     
-    for prefix in Prefix: 
+    for prefix in prefix_list: 
         for obj in bucket.objects.filter(Prefix=prefix):
             # Here we are copying the directory structure from s3 bucket.
             if not os.path.exists(os.path.join('data',os.path.dirname(obj.key))):
                 os.makedirs(os.path.join('data',os.path.dirname(obj.key)))
-            
+                
             dest_path = os.path.join('data',obj.key)
             try:
                 bucket.download_file(obj.key,dest_path)
